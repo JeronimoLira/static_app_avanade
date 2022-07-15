7940469e720afac64eaf5f53bbf4ec44945d2e58
Jeronimo Lira <jeronimo@pereiralira.com.br>
2022-07-15 17:32:49 -0300

    Update - Tentativa mostrar logos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from './avanade_logo.png';
+import logo from './logo192.png';
+import logo from './logo512.png';
 import './App.css';
 import { useEffect, useState } from 'react'
 import axios from 'axios'; 
